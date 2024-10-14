0d9755e9d13f29f8940bc246221948c24dd0a364
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-10-14 07:47:26 +0545

    file

--- advancedInterfaces.go
+++ advancedInterfaces.go
@@ -1,61 +1,61 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Mammals interface {
-	feedMilk() string
-}
+// type Mammals interface {
+// 	feedMilk() string
+// }
 
-type Birds interface {
-	laysEggs() string
-}
+// type Birds interface {
+// 	laysEggs() string
+// }
 
-type Bat interface {
-	Mammals
-	Birds
-}
+// type Bat interface {
+// 	Mammals
+// 	Birds
+// }
 
-type Animals struct {
-	kingdom string
-	class   string
-}
+// type Animals struct {
+// 	kingdom string
+// 	class   string
+// }
 
-type animalError struct {
-	msg string
-}
+// type animalError struct {
+// 	msg string
+// }
 
-func (a Animals) feedMilk() string {
-	if a.class != "bird" {
-		return "Yes the feed milk"
-	} else {
-		return "no they dont feed milk"
-	}
-}
+// func (a Animals) feedMilk() string {
+// 	if a.class != "bird" {
+// 		return "Yes the feed milk"
+// 	} else {
+// 		return "no they dont feed milk"
+// 	}
+// }
 
-func (a Animals) laysEggs() string {
-	if a.class != "mammals" {
-		return "They lay eggs"
-	}
-	return "Thy don't lay eggs"
-}
+// func (a Animals) laysEggs() string {
+// 	if a.class != "mammals" {
+// 		return "They lay eggs"
+// 	}
+// 	return "Thy don't lay eggs"
+// }
 
-func check(a Animals) {
-	fmt.Println(a.feedMilk())
-	fmt.Println(a.laysEggs())
-}
+// func check(a Animals) {
+// 	fmt.Println(a.feedMilk())
+// 	fmt.Println(a.laysEggs())
+// }
 
-func main() {
+// func main() {
 
-	pigeon := Animals{
-		kingdom: "animal",
-		class:   "bird",
-	}
-	hippo := Animals{
-		kingdom: "animal",
-		class:   "mammals",
-	}
+// 	pigeon := Animals{
+// 		kingdom: "animal",
+// 		class:   "bird",
+// 	}
+// 	hippo := Animals{
+// 		kingdom: "animal",
+// 		class:   "mammals",
+// 	}
 
-	check(pigeon)
-	check(hippo)
+// 	check(pigeon)
+// 	check(hippo)
 
-}
+// }
