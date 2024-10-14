7e0fcc9d2fb773b93d09350eb4f017f52506760a
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-10-14 08:02:18 +0545

    web request

---
+++ webRequets.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const url = "https://google.com"
+
+func main() {
+	fmt.Println("web request")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response i :%T", response)
+
+	defer response.Body.Close() //callers responsibility to close the request
+
+	databytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+
+	fmt.Println(content)
+}
