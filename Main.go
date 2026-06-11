dc53aa4184fd558cb15f28b06882aa326b1553be
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-06-11 18:35:50 +0545

    2026/06/11

---
+++ Main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//
+//func myFunc(x, y int) (int, error) {
+//	return x + y, nil
+//}
+
+func main() {
+	//num, err := myFunc(10, 20)
+	//fmt.Println(num, err)
+
+	a := 2
+	z := a > 1
+	var age int
+	fmt.Println("Please Enter your age:")
+	_, err := fmt.Scan(&age)
+	if err != nil {
+		return
+	}
+
+	if age < 18 {
+		fmt.Println("Are you dead or what!")
+	} else {
+		fmt.Println("Your seem eligible for the liscense")
+	}
+	if z == true {
+		fmt.Println("Hello, World!")
+	} else {
+		fmt.Println("Hello, World! but in false")
+	}
+
+	fmt.Println(z)
+
+}
