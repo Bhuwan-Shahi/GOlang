b896c66fe36bd6a284172c64057b0f2b0de42f82
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-14 15:18:39 +0545

    InterfaceS - The Payment Gateway

--- interfacesP.go
+++ interfacesP.go
@@ -28,10 +28,45 @@ func (c Circle) Area() float64 {
 func PrintArea(s Shape) {
 	fmt.Println("The areas :is ", s.Area())
 }
+
+type PaymentProcessor interface {
+	Pay(amount float64) bool
+}
+type PayPal struct {
+	Email string
+}
+
+type CreditCard struct {
+	CardNumber string
+}
+
+func (p PayPal) Pay(amount float64) bool {
+	fmt.Println("Paid $", amount, "using PayaPal Account:", p.Email)
+	return true
+}
+func (c CreditCard) Pay(amount float64) bool {
+	lastfour := "xxxx"
+	if len(c.CardNumber) >= 4 {
+		lastfour = c.CardNumber[len(c.CardNumber)-4:]
+	}
+	fmt.Println("Charged $", amount, "to card ending in: ", lastfour)
+	return true
+}
+
+func Checkout(p PaymentProcessor, total float64) {
+	p.Pay(total)
+}
+
 func main() {
+
+	p := PayPal{"Ramprasd@gmail.com"}
+	c := CreditCard{"1233345"}
+
+	Checkout(p, 123.2)
+	Checkout(c, 123.2)
+
 	R := Rectangle{1, 2}
 	C := Circle{2}
 	PrintArea(R)
 	PrintArea(C)
-
 }
