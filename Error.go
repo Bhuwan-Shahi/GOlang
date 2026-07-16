e4257a829346512bbef333a33fad266eb5dd2d8b
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-16 11:53:58 +0545

    PareseAGe

--- Error.go
+++ Error.go
@@ -3,7 +3,16 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
+
+func ParseAge(input string) (int, error) {
+	integer, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, fmt.Errorf("invalide age format : %w", err)
+	}
+	return integer, nil
+}
 
 func div(a int, b int) (int, error) {
 	if b == 0 {
@@ -20,6 +29,7 @@ func deffered() {
 }
 
 func main() {
+	fmt.Println(ParseAge("bhuwan"))
 	result, err := div(2, 2)
 	if err != nil {
 		fmt.Println("Error Occured")
