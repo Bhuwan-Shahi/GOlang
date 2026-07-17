cb4330dee7cb09aa3dd0a467972aecda689d5285
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-17 16:09:48 +0545

    Pointer in struct

--- Pointers.go
+++ Pointers.go
@@ -2,13 +2,35 @@ package main
 
 import "fmt"
 
+type Book struct {
+	id    int
+	title string
+}
+
+func (b *Book) setTitle(title string) {
+	b.title = title
+}
+
+func change(x *int) {
+	*x = 100
+	fmt.Println(*x)
+}
+
 func main() {
+	b := Book{1, "Bhuwan ko Book"}
+	b.setTitle("Bhuwan ko book aaba ram ko vayo because pointer ko refrence")
+	fmt.Println(b)
 	x := 0
 	fmt.Println(x)
 	y := &x
+	z := &y
 
 	*y = 8
+	fmt.Println(**z)
 
 	fmt.Println(x)
+	yy := 1
 
+	change(&yy)
+	println(yy)
 }
