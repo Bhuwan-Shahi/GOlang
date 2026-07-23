a1d66e93b2cba8505afdf28f54ad3186d5e278e2
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-23 18:54:35 +0545

    dynamic delays

--- Channels.go
+++ Channels.go
@@ -1,24 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func add(a, b int, ch chan int) {
+func add(a, b int, ch chan int, delay int) {
+	time.Sleep(time.Duration(delay) * time.Second)
 	fmt.Println(a + b)
 	ch <- a + b
 }
 
 func main() {
-	ch := make(chan int)
-	go add(2, 3, ch)
-	x := <-ch
+	//ch := make(chan int)
+	//go add(2, 3, ch)
+	//x := <-ch
+	//
+	//go add(6, 3, ch)
+	//x = <-ch
+	//go add(5, 3, ch)
+	//x = <-ch
+	//go add(4, 3, ch)
+	//x = <-ch
+	//go add(3, 3, ch)
+	//x = <-ch
+	//fmt.Println(x)
+	//
 
-	go add(6, 3, ch)
-	x = <-ch
-	go add(5, 3, ch)
-	x = <-ch
-	go add(4, 3, ch)
-	x = <-ch
-	go add(3, 3, ch)
-	x = <-ch
-	fmt.Println(x)
+	fmt.Println("Adding Mulitple Channels")
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go add(1, 2, ch1, 1)
+	go add(3, 2, ch2, 2)
+
+	x := <-ch2
+	y := <-ch1
+
+	fmt.Println(x, y)
 }
