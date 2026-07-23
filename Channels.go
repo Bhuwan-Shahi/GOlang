52943ddbf1dbb25b3285c719d2fabb22f835c390
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-23 18:45:22 +0545

    same  channel used multipletime

--- Channels.go
+++ Channels.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func add(a, b int, ch chan int) {
+	fmt.Println(a + b)
 	ch <- a + b
 }
 
@@ -10,5 +11,14 @@ func main() {
 	ch := make(chan int)
 	go add(2, 3, ch)
 	x := <-ch
+
+	go add(6, 3, ch)
+	x = <-ch
+	go add(5, 3, ch)
+	x = <-ch
+	go add(4, 3, ch)
+	x = <-ch
+	go add(3, 3, ch)
+	x = <-ch
 	fmt.Println(x)
 }
