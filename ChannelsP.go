236a996bdd95bc4bc07625ab767a78da565c313a
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-24 21:31:53 +0545

    practice but tired

--- ChannelsP.go
+++ ChannelsP.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -18,17 +19,40 @@ func Ssum(arr []int, ch chan int) {
 	ch <- sum
 }
 
-func main() {
+var ErrInvalidAmoun = errors.New("Amount must be positive!")
 
-	ch := make(chan int)
-	arr := []int{1, 2, 3}
-	arr1 := []int{4, 5, 6}
-	go Ssum(arr, ch)
-	go Ssum(arr1, ch)
-	x := <-ch
-	y := <-ch
-	fmt.Println("the sum of the array is:")
-	fmt.Println(x + y)
+type Account struct {
+	Id      string
+	Balance float64
+}
+
+func (acc *Account) deposit(amount float64) error {
+	if acc == nil {
+		return errors.New("Nil account pointer")
+	}
+	if amount <= 0 {
+		return fmt.Errorf("deposit failed for account %s: %w", acc.Id, ErrInvalidAmoun)
+	}
+	acc.Balance += amount
+	return nil
+
+}
+
+func main() {
+	acc := Account{"Bhuwan", 0.0}
+
+	fmt.Println(acc.deposit(12))
+	fmt.Println(acc.Balance)
+	//
+	//ch := make(chan int)
+	//arr := []int{1, 2, 3}
+	//arr1 := []int{4, 5, 6}
+	//go Ssum(arr, ch)
+	//go Ssum(arr1, ch)
+	//x := <-ch
+	//y := <-ch
+	//fmt.Println("the sum of the array is:")
+	//fmt.Println(x + y)
 	//x := <-ch
 	//
 	//fmt.Println(x)
