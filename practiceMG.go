64aa5edfa8d571050b98b0b54b8d19f647385c3c
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2026-07-28 15:12:00 +0545

    geminitcapstone

--- practiceMG.go
+++ practiceMG.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type counts struct {
+	counter int
+	lock    sync.Mutex
+}
+
+func Increment(c *counts, wg *sync.WaitGroup) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.counter++
+	fmt.Println(c.counter)
+	wg.Done()
+
+}
+func main() {
+	c := counts{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go Increment(&c, &wg)
+	}
+	wg.Wait()
+}
